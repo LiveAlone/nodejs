7142a53f3d3c3008a171ebfb0cb6cf3a248c45c3
yaoqijun <yaoqijunmail@foxmail.com>
2017-02-22 22:06:08 +0800

    add all

---
+++ js/Number.js
@@ -0,0 +1,24 @@
+console.log('this is number content test');
+
+// console.log(0b111110111 === 503); // true
+// console.log(0o767 === 503) // true
+
+// console.log(Number.isFinite(15)); // true
+// console.log(Number.isFinite(0.8)); // true
+// console.log(Number.isFinite(NaN)); // false
+// console.log(Number.isFinite(Infinity)); // false
+// console.log(Number.isFinite(-Infinity)); // false
+// console.log(Number.isFinite('foo')); // false
+// console.log(Number.isFinite('15')); // false
+// console.log(Number.isFinite(true)); // false
+
+// 校验 NaN 判定方式
+// console.log(Number.isNaN(NaN)); // true
+// console.log(Number.isNaN(15)); // false
+// console.log(Number.isNaN('15')); // false
+// console.log(Number.isNaN(true)); // false
+// console.log(Number.isNaN(9/NaN)); // true
+// console.log(Number.isNaN('true'/0)); // true
+// console.log(Number.isNaN('true'/'true')); // true
+
+
