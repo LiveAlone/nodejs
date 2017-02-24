8b55cb05f755355d266f71eb4e7ce11954a4d175
yaoqijun <yaoqijun@changingedu.com>
2017-02-24 11:50:16 +0800

    test

--- js/Iterator.js
+++ js/Iterator.js
@@ -1,10 +1,25 @@
-function makeIterator(array) {
-  var nextIndex = 0;
-  return {
-    next: function() {
-      return nextIndex < array.length ?
-        {value: array[nextIndex++], done: false} :
-        {value: undefined, done: true};
-    }
-  };
-}
+// function makeIterator(array) {
+//   var nextIndex = 0;
+//   return {
+//     next: function() {
+//       return nextIndex < array.length ?
+//         {value: array[nextIndex++], done: false} :
+//         {value: undefined, done: true};
+//     }
+//   };
+// }
+
+// var it = makeIterator([1,2,3,4]);
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+
+let arr = ['a','b','c','d'];
+let iter = arr[Symbol.iterator]();
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
